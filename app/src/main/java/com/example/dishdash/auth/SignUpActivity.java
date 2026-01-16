b3519affe10650bfd22203898242d22b460fe4cb
Ahmed Sayed <149809000+ahmedSayed321@users.noreply.github.com>
2026-01-16 23:12:48 +0200

    finish sign up ui

--- app/src/main/java/com/example/dishdash/auth/SignUpActivity.java
+++ app/src/main/java/com/example/dishdash/auth/SignUpActivity.java
@@ -1,6 +1,10 @@
 package com.example.dishdash.auth;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,15 +15,30 @@ import com.example.dishdash.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    ImageView backBtn;
+    EditText firstName , lastName , password , confirmPassword;
+    Button signUpBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        backBtn = findViewById(R.id.backBtn);
+        firstName = findViewById(R.id.editTextFirstName);
+        lastName = findViewById(R.id.editTextLastName);
+        password = findViewById(R.id.editTextPassword);
+        confirmPassword = findViewById(R.id.editTextConfirmPassword);
+        signUpBtn =findViewById(R.id.signupBtn);
+
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
